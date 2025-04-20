d863ef12d721b3873131876dfd8c3371faff105a
TankVivek <vivektank136@gmail.com>
2025-04-21 01:19:05 +0530

    Update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT || '5173'),
+    host: true
+  },
   preview: {
+    port: parseInt(process.env.PORT || '3000'),
+    host: true,
     allowedHosts: ['techcube-in.onrender.com', 'techcube.in']
   }
 })
