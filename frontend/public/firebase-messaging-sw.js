16686f3ac18bcb64bcdeeb80fb1462068ebf27a0
TankVivek <vivektank136@gmail.com>
2026-06-07 00:16:39 +0530

    feat: add environment configuration templates and Firebase service worker for push notifications

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -7,7 +7,8 @@ const firebaseConfig = {
   projectId: "techcube-99abf",
   storageBucket: "techcube-99abf.firebasestorage.app",
   messagingSenderId: "814216803952",
-  appId: "1:814216803952:web:b6cd67c0d7283e67f0e6b5"
+  appId: "1:814216803952:web:b6cd67c0d7283e67f0e6b5",
+  measurementId: "G-PYE4J9VXSD"
 };
 
 firebase.initializeApp(firebaseConfig);
